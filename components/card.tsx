6322259a42e7e6ab3b8ba7bf23e066ff37acebd3
Simon Huang <simon.huang@tum.de>
2022-09-03 23:27:32 +0200

    refactor to timestamps

--- components/card.tsx
+++ components/card.tsx
@@ -2,7 +2,7 @@ interface cardProps {
   headline: string;
   location: string;
   subheadline: string;
-  date: string;
+  date: string[];
   description?: string[] | string;
 }
 
@@ -15,9 +15,38 @@ const Card = (props: cardProps) => (
     <p className="text-sm">{props.subheadline}</p>
 
     <p>
-      {props.location}, {props.date}
+      {props.location}, {prettyDates(props.date)}
     </p>
   </div>
 );
+
+const prettyDates = (dates: string[]) => {
+  const prettyDate = (dateString: string) => {
+    //TODO localization
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    if (dateString === "now") return "now";
+
+    const date = new Date(dateString);
+
+    return `${months[date.getMonth()]} ${date.getFullYear()}`;
+  };
+
+  console.log(dates);
+  return dates.map((date) => prettyDate(date)).join(" - ");
+};
 
 export default Card;
