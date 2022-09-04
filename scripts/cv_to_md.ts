03184d5997cfbe8b57bbe1766cb732ae4fe2bcd6
Simon Huang <simon.huang@tum.de>
2022-09-04 14:30:05 +0200

    add script to parse json into md

--- scripts/cv_to_md.ts
+++ scripts/cv_to_md.ts
@@ -1,7 +1,36 @@
 import json2md from "json2md";
 import cv from "../assets/cv.json";
+import fs from "fs";
+import prettyDates from "../util/prettyDates";
 
 // Reads from cv.json and writes to a markdown file in pages,
 // so that the route /cv is available
 
-console.log(json2md([{ h1: cv.name }]));
+interface cvItem {
+  headline: string;
+  location: string;
+  subheadline: string;
+  date: string[];
+  description: string[];
+}
+
+const cvItemTemplate = (item: cvItem) => {
+  return [
+    { h3: `${item.headline}, ${item.location} – ${item.subheadline}` },
+    { p: prettyDates(item.date) },
+    { ul: item.description },
+  ];
+};
+
+const cvTemplate = json2md([
+  { h1: cv.name },
+  { h2: "Work experience" },
+  cv.work.map((item) => cvItemTemplate(item)),
+  { h2: "Education" },
+  cv.education.map((item) => cvItemTemplate(item)),
+]);
+
+fs.writeFile("pages/cv.md", cvTemplate, (err) => {
+  if (err) return console.log(err);
+  console.log("Succesfully written to pages/cv.md");
+});
