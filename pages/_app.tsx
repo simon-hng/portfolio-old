661696c0b15136e857ee45b330376532ad8b7c57
Simon Huang <simon.huang@tum.de>
2022-09-04 20:39:37 +0200

    move theme back into layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import Layout from "../components/layout";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 };
 
 export default MyApp;
