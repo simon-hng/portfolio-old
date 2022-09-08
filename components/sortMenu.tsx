16bcb5b01d00d50abe0ad11e7a61ce1f8c331025
Simon Huang <simon.huang@tum.de>
2022-09-09 01:29:55 +0200

    replace with fontawesome icons

--- components/sortMenu.tsx
+++ components/sortMenu.tsx
@@ -1,11 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faXmark,
+  faBars,
+  faArrowDown,
+} from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useRef, useState } from "react";
 import { IconButton } from "./iconButton";
-import {
-  sortMenuProps,
-  sortings,
-  SortDirectionIcon,
-  sortingsType,
-} from "./resume";
+import { sortMenuProps, sortings, sortingsType } from "./resume";
 
 export const SortMenu = ({
   sortedBy,
@@ -56,7 +57,12 @@ export const SortMenu = ({
                 }}
                 text={sort}
               >
-                {sortedBy === sort && <SortDirectionIcon sortAsc={sortAsc} />}
+                {sortedBy === sort && (
+                  <FontAwesomeIcon
+                    icon={faArrowDown}
+                    className={`${sortAsc && "rotate-180"} duration-300 w-4`}
+                  />
+                )}
               </IconButton>
             </li>
           ))}
@@ -94,27 +100,9 @@ const SortMenuButton = ({
       text={menuOpen ? "no sort" : "sort by"}
     >
       {menuOpen ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 0 24 24"
-          width="24px"
-          className="w-6"
-        >
-          <path d="M0 0h24v24H0V0z" fill="none" />
-          <path d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z" />
-        </svg>
+        <FontAwesomeIcon icon={faXmark} className="w-4 h-6" />
       ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 0 24 24"
-          width="24px"
-          className="w-6"
-        >
-          <path d="M0 0h24v24H0V0z" fill="none" />
-          <path d="M4 18h4c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1zm1 6h10c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1z" />
-        </svg>
+        <FontAwesomeIcon icon={faBars} className="w-4" />
       )}
     </IconButton>
   );
