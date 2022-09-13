0f2589ec5778fed12aceda6ebfa0e31d5297149a
Simon Huang <simon.huang@tum.de>
2022-09-13 04:55:59 +0200

    refactor hero into own component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import Head from "next/head";
+import Hero from "../components/hero";
 import Resume from "../components/resume";
-import Image from "next/image";
 import Skills from "../components/skills";
-
-const Link = ({ href, text, className }) => (
-  <a className={"duration-500 font-bold text-text " + className} href={href}>
-    {text}
-  </a>
-);
 
 const Home = () => (
   <>
@@ -19,42 +13,7 @@ const Home = () => (
 
     <div className="flex justify-center">
       <div>
-        <div
-          className="my-20 flex flex-col space-y-5 items-center 
-            md:flex-row md:space-x-5"
-        >
-          <div className="rounded-full overflow-clip w-48 h-48 shrink-0">
-            <Image
-              src="https://avatars.githubusercontent.com/u/25489463?s=400&u=669e0467028bbd791a9a24e718ee7927c87edaca&v=4"
-              alt="Image of my face"
-              width="192"
-              height="192"
-              draggable="false"
-            />
-          </div>
-
-          <div className="flex flex-col text-center justify-center md:text-left">
-            <h1 className="text-6xl mb-5 text-text">
-              Hello, I&apos;m&nbsp;Simon
-            </h1>
-
-            <p className="text-xl">
-              Informatics student at{" "}
-              <Link
-                href="https://www.in.tum.de/"
-                text="TUM"
-                className="hover:text-tum-blue"
-              />
-              , working student at{" "}
-              <Link
-                href="https://www.siemens.com"
-                text="Siemens"
-                className="hover:text-siemens-petrol"
-              />
-              .
-            </p>
-          </div>
-        </div>
+        <Hero />
 
         <Resume />
 
